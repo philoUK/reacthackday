7c2c25dab3782e3ccf25cdb7451bc253f646fcce
David Gruar <davidgruar@gmail.com>
2016-09-30 11:09:01 +0100

    Initial state and update methods

--- src/Components/LeagueTable.js
+++ src/Components/LeagueTable.js
@@ -1,17 +1,68 @@
 import React, { PropTypes } from 'react'
 
 class LeagueTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      teams: [],
+      matches: [],
+      rules: {
+        pointsForWin: 3,
+        pointsForDraw: 1
+      }
+    };
+
+    this.actions = {
+      addTeam: this.addTeam.bind(this),
+      addMatch: this.addMatch.bind(this),
+      setPointsForWin: this.setPointsForWin.bind(this),
+      setPointsForDraw: this.setPointsForDraw.bind(this)
+    };
+  }
+
+  addTeam(name) {
+    this.setState({
+      teams: this.state.teams.concat(name)
+    });
+  }
+
+  setPointsForWin(points) {
+    this.setState({
+      rules: {
+        pointsForWin: points,
+        pointsForDraw: this.state.rules.pointsForDraw
+      }
+    });
+  }
+
+  setPointsForDraw(points) {
+    this.setState({
+      rules: {
+        pointsForWin: this.state.rules.pointsForWin,
+        pointsForDraw: points
+      }
+    });
+  }
+
+  addMatch(match) {
+    this.setState({
+      matches: this.state.matches.concat(match)
+    });
+  }
+
   render () {
     return (
 
       <div className="league-table">
-        <AddMatch />
+        {/*
+        <AddMatch teams={this.state.teams} actions={this.actions} />
         <ResultsTable />
         <div className="settings">
-          <LeagueRules />
-          <LeagueRules />
-          <AddTeam />
+          <LeagueRules actions={this.actions} />
+          <LeagueRules actions={this.actions} />
+          <AddTeam actions={this.actions} />
         </div>
+        */}
       </div>
     )
   }
